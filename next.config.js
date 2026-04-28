832d4c3daea470e213d457c98624cafc7040d1b5
Pubudu Tharanga <pubudutharange@gmail.com>
2026-04-28 11:59:39 +0530

    Fix Vercel deployment asset paths

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ module.exports = {
   images: {
     unoptimized: true
   },
-  basePath: '/Sri-Lanka-train-timetable',
-  assetPrefix: '/Sri-Lanka-train-timetable/',
   env: {
     NEXT_PUBLIC_GA_MEASUREMENT_ID: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || 'G-FLEV55JTJJ'
   }
